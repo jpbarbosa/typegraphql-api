1af0e7d1310198a6420a5a589571c6699f395397
JP Barbosa <jp@jp7.com.br>
2022-01-18 11:57:49 -0300

    TypeGraphQL

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Application is up and running!');
+import { ApolloServer } from 'apollo-server';
+import { createSchema } from './utils/createSchema';
+
+const port = 4000;
+
+const main = async () => {
+  const schema = await createSchema();
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  server.listen(port, () => {
+    console.log(`Server has started on port ${port}`);
+  });
+};
+
+main();
